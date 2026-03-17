cedcc57f4581e95d4de4996b4037389f206899b1
pwc-lavin-liu <lavin.liu-ext@loropiana.com>
2026-03-17 15:24:53 +0800

    1

--- src/detect/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Variance detection package for financial metrics."""
-
-from src.detect.detector import detect_variances
-from src.detect.models import DetectionResult, MetricConfig
-
-__all__ = ["detect_variances", "DetectionResult", "MetricConfig"]
